e0bf4fd4977ec00f104edd8ccace9746e519a649
Jason Chen <jianxinchen298@gmail.com>
2024-02-09 19:49:30 -0500

    "Let's get started" page is in main.dart.
    Can swap to the onboarding view with elevated button class. Still has the fix the
    width of the button. Next step is to add the images for the two onboarding views which will use a
    page controller to swipe right. Then add the login page. The exercise/training page should be
    connected to bluetooth, which Janroy is currently working on.

--- risestrideapp/lib/main.dart
+++ risestrideapp/lib/main.dart
@@ -1,25 +1,175 @@
 import 'package:flutter/material.dart';
-import 'package:risestride/onboarding/onboarding_view.dart';
+// import 'package:risestride/onboarding/onboarding_view.dart';
 import 'package:risestride/pages/home.dart';
 import 'common/colors_extension.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const MaterialApp(
+      home: Start(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+Route _createRoute() {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => Home(),
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      return child;
+    },
+  );
+}
 
-  // This widget is the root of your application.
+class Start extends StatelessWidget {
+  const Start({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: "Rise Stride",
-        theme: ThemeData(
-          primaryColor: ThemeColors.primaryColor1,
-          fontFamily: "Poppins",
-        ),
-        home: const OnBoardingView()); //MaterialApp
+    var media = MediaQuery.of(context).size;
+    return Scaffold(
+        appBar: AppBar(),
+        body: SizedBox(
+            width: media.width,
+            height: media.height,
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+              const Padding(
+                padding: EdgeInsets.only(top: 300),
+                child: Text("RiseStride",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        fontStyle: FontStyle.italic)),
+              ),
+              const SizedBox(width: 100, height: 100),
+              Center(
+                  child: Container(
+                      child: Padding(
+                          padding: EdgeInsets.only(top: 100),
+                          child: ElevatedButton(
+                              onPressed: () {
+                                Navigator.of(context).push(_createRoute());
+                              },
+                              style: ElevatedButton.styleFrom(
+                                  shadowColor: Colors.transparent,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(20))),
+                              child: Ink(
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.all(
+                                          Radius.circular(30.0)),
+                                      gradient: LinearGradient(
+                                          begin: Alignment.topLeft,
+                                          end: Alignment(0.2, 1),
+                                          colors: <Color>[
+                                            ThemeColors.primaryColor2,
+                                            ThemeColors.primaryColor1,
+                                          ])),
+                                  child: Container(
+                                      constraints: const BoxConstraints(
+                                          maxWidth: 160,
+                                          minWidth: 80.0,
+                                          minHeight: 38.0),
+                                      alignment: Alignment.center,
+                                      child: const Text("Let's Get Started",
+                                          style: TextStyle(
+                                              color: Colors.black,
+                                              fontSize: 15,
+                                              fontWeight: FontWeight.bold))))
+                              // child: Container(
+                              //     width: media.width * .6,
+                              //     height: media.height * .05,
+                              //     decoration: BoxDecoration(
+                              //         gradient: LinearGradient(
+                              //             begin: Alignment.topLeft,
+                              //             end: Alignment(0.2, 1),
+                              //             colors: <Color>[
+                              //               ThemeColors.primaryColor2,
+                              //               ThemeColors.primaryColor1,
+                              //             ]),
+                              //         borderRadius: BorderRadius.circular(20)),
+                              //     child: const Padding(
+                              //       padding: EdgeInsets.all(8.0),
+                              //       child: Text("Let's Get Started!",
+                              //           textAlign: TextAlign.center,
+                              //           style: TextStyle(
+                              //               fontSize: 15, fontWeight: FontWeight.bold)),
+                              //     ))
+                              ))))
+            ])));
+  }
+}
+// body: Container(child:Center(
+//     child: ElevatedButton(
+//         onPressed: () {
+//           Navigator.of(context).push(_createRoute());
+//         },
+//         child: SizedBox(
+//             width: media.width,
+//             height: media.height,
+//             child: const Column(
+//                 mainAxisAlignment: MainAxisAlignment.start,
+//                 children: [
+//                   Padding(
+//                     padding: EdgeInsets.only(top: 300),
+//                     child: Text("RiseStride",
+//                         textAlign: TextAlign.center,
+//                         style: TextStyle(
+//                             fontSize: 30,
+//                             fontWeight: FontWeight.bold,
+//                             fontStyle: FontStyle.italic)),
+//                   ),
+//                 ]))))));
+// child: SizedBox(
+//     width: media.width,
+//     height: media.height,
+//     child: const Column(
+//         mainAxisAlignment: MainAxisAlignment.start,
+//         children: [
+//           Padding(
+//             padding: EdgeInsets.only(top: 300),
+//             child: Text("RiseStride",
+//                 textAlign: TextAlign.center,
+//                 style: TextStyle(
+//                     fontSize: 30,
+//                     fontWeight: FontWeight.bold,
+//                     fontStyle: FontStyle.italic)),
+//           ),
+// GestureDetector(
+//     onTap: () {},
+//     child: Container(
+//         width: media.width * .6,
+//         height: media.height * .05,
+//         margin: EdgeInsets.only(top: 300),
+//         decoration: BoxDecoration(
+//             gradient: LinearGradient(
+//                 begin: Alignment.topLeft,
+//                 end: Alignment(0.2, 1),
+//                 colors: <Color>[
+//                   ThemeColors.primaryColor2,
+//                   ThemeColors.primaryColor1,
+//                 ]),
+//             borderRadius: BorderRadius.circular(20)),
+//         child: const Padding(
+//           padding: EdgeInsets.all(8.0),
+//           // child: Text("Let's Get Started!",
+//           //     textAlign: TextAlign.center,
+//           //     style: TextStyle(
+//           //         fontSize: 15, fontWeight: FontWeight.bold)),
+
+//         )))
+
+class Page2 extends StatelessWidget {
+  const Page2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Center(
+        child: Text('Page 2'),
+      ),
+    );
   }
 }
